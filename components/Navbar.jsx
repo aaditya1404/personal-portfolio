3b503aa9f2900ef06090be22a13307d86e0e2f1e
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2025-10-22 21:04:14 +0530

    Bugs fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,13 @@ import { FiHome } from "react-icons/fi";
 const Navbar = () => {
     return (
         <div className="flex w-full backdrop-blur-sm fixed items-center justify-center gap-4 h-20 lg:h-20 border-b border-black/10">
-            <Link href={"/#work"} className="cursor-pointer translate-x-4">Work</Link>
-            <Link href={"/#skill"} className="cursor-pointer translate-x-4">Skill</Link>
+            <Link href={"/#work"} className="cursor-pointer translate-x-4 py-7">Work</Link>
+            <Link href={"/#skill"} className="cursor-pointer translate-x-4 py-7">Skill</Link>
             <div className="translate-x-4">
                 <FiHome className="text-2xl cursor-pointer"/>
             </div>
-            <p className="cursor-pointer translate-x-4">About</p>
-            <p className="cursor-pointer translate-x-4">Contact</p>
+            <p className="cursor-pointer translate-x-4 py-7">About</p>
+            <p className="cursor-pointer translate-x-4 py-7">Contact</p>
         </div>
     )
 }
