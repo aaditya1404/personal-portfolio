f87e48bf6620f45ceb5b7fbd91ceb23eb2405273
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2025-10-23 23:55:46 +0530

    Bugs fixed

--- components/SmartPhoneFrame.jsx
+++ components/SmartPhoneFrame.jsx
@@ -10,7 +10,7 @@ const SmartPhoneFrame = ({ src, onClose }) => {
             >
                 Back
             </button>
-            <div className='relative bg-black rounded-4xl p-2 w-[320px] h-[690px] lg:w-[280px] lg:h-[600px] lg:mt-0 overflow-hidden border-2 border-gray-700 shadow-2xl '>
+            <div className='relative bg-black rounded-4xl p-2 w-[320px] h-[650px] lg:w-[280px] lg:h-[600px] lg:mt-0 overflow-hidden border-2 border-gray-700 shadow-2xl '>
                 <iframe src={src} className='w-full h-full rounded-3xl border-none' title='Project Demo'></iframe>
             </div>
         </div>
